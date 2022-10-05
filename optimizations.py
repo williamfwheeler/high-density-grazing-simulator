1125bd059156c748adc5b30ab80e73e3ccc3fd3d
William Wheeler <williamwheelertx@gmail.com>
2022-10-05 16:19:12 -0500

    add initial optimization building blocks

---
+++ optimizations.py
@@ -0,0 +1,34 @@
+from high_density_grazing import Plot, Paddock, Herd
+
+class Optimizations:
+
+    '''ADD DESCRIPTIONS AND COMMENTARY'''
+
+    def potential_herd_weight(self,plot,no_of_paddocks,proxy_herd,proxy_paddock,target_density,target_utilization):
+    
+        plotlengthneeded = proxy_paddock.regrowth_period/(no_of_paddocks-1)
+        
+        forage_per_paddock = (proxy_paddock.forage_height*proxy_paddock.dry_matter_per_inch_acre*target_utilization)*(plot.total_acreage/
+                                                                                            no_of_paddocks)
+        
+        daily_forage_available = forage_per_paddock / plotlengthneeded
+        
+        potential_herd_weight = daily_forage_available/proxy_herd.body_weight_eaten
+        
+        return round(potential_herd_weight,0)
+
+
+
+    
+    def acres_needed(self,herd,no_of_paddocks,proxy_paddock,target_density,target_utilization):
+    
+        plotlengthneeded = proxy_paddock.regrowth_period/(no_of_paddocks-1)
+        
+        herd90dayneed = herd.herd_forage_need*plotlengthneeded
+        
+        acres_needed_per_paddock = herd90dayneed/(proxy_paddock.forage_height*
+                                            proxy_paddock.dry_matter_per_inch_acre*target_utilization)
+        
+        acres_needed = acres_needed_per_paddock * no_of_paddocks
+        
+        return round(acres_needed,2) #, no_of_paddocks, acres_needed_per_paddock
